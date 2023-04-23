3eae2766b765d644ceddc8311ab5bb9cf3b4ba94
Myproject <mohamed.alaa@gmail.com>
2023-04-23 15:25:33 -0700

    add_API

---
+++ lib/API_weather/API.dart
@@ -0,0 +1,5 @@
+ // https://api.weatherapi.com/v1/forecast.json?key=3bb25b765aa64660b6700716232404&q=London&days=7&aqi=no8alerts=no
+
+ // base url : https://api.weatherapi.com/v1/
+ // method(url) : forecast.json
+ // API KEY : 3bb25b765aa64660b6700716232404
