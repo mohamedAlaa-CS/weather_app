e11ae55806bea9e194387cfd5c8ad7f0046f52de
Myproject <mohamed.alaa@gmail.com>
2023-05-05 19:16:17 -0700

    home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:weatheer_app/models/weather_model.dart';
 import 'package:weatheer_app/pages/searchScreen.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
-
+  HomeScreen({Key? key}) : super(key: key);
+  WeatherModel? weatherDAta;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,25 +20,70 @@ class HomeScreen extends StatelessWidget {
               icon: Icon(Icons.search))
         ],
       ),
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text(
-              'there is no weather 😔 start',
-              style: TextStyle(
-                fontSize: 27,
-              ),
-            ),
-            Text(
-              'searching now 🔍',
-              style: TextStyle(
-                fontSize: 27,
+      body: weatherDAta != null
+          ? Center(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    'there is no weather 😔 start',
+                    style: TextStyle(
+                      fontSize: 27,
+                    ),
+                  ),
+                  Text(
+                    'searching now 🔍',
+                    style: TextStyle(
+                      fontSize: 27,
+                    ),
+                  )
+                ],
               ),
             )
-          ],
-        ),
-      ),
+          : Container(
+              color: Colors.orange,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Spacer(
+                    flex: 3,
+                  ),
+                  Text('Cairo',
+                      style:
+                          TextStyle(fontSize: 32, fontWeight: FontWeight.bold)),
+                  Text(
+                    'update:5-5-2023',
+                    style: TextStyle(
+                      fontSize: 22,
+                    ),
+                  ),
+                  Spacer(),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Image.asset('lib/assets/images/clear.png'),
+                      Text(
+                        '30',
+                        style: TextStyle(
+                          fontSize: 32,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Column(
+                        children: [Text('MaxTemp:30'), Text('MiniTemp:20')],
+                      ),
+                    ],
+                  ),
+                  Spacer(),
+                  Text('Clear',
+                      style:
+                          TextStyle(fontSize: 32, fontWeight: FontWeight.bold)),
+                  Spacer(
+                    flex: 5,
+                  ),
+                ],
+              ),
+            ),
     );
   }
 }
